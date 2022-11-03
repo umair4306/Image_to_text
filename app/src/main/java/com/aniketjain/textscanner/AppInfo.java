054502285bafcf244560360f57055ef76d0c9f2d
umair4306 <mumair4306@gmail.com>
2022-11-03 19:12:40 +0500

    SplashScreen and Onbording Screen Added

--- app/src/main/java/com/aniketjain/textscanner/AppInfo.java
+++ app/src/main/java/com/aniketjain/textscanner/AppInfo.java
@@ -1,15 +1,100 @@
 package com.aniketjain.textscanner;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.applovin.mediation.MaxAd;
+import com.applovin.mediation.MaxAdFormat;
+import com.applovin.mediation.MaxAdViewAdListener;
+import com.applovin.mediation.MaxError;
+import com.applovin.mediation.ads.MaxAdView;
+import com.applovin.sdk.AppLovinSdkUtils;
 
 public class AppInfo extends AppCompatActivity {
 
+    private MaxAdView MRECAdview;
+    ImageView icBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_info);
 
+
+        icBack = findViewById(R.id.icBack);
+        createMrecAd();
+
+        icBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AppInfo.this,MainActivity.class));
+                finish();
+            }
+        });
     }
+    private void createMrecAd() {
+        MRECAdview = new MaxAdView(getResources().getString(R.string.mrec), MaxAdFormat.MREC, this);
+        MRECAdview.setListener(new MaxAdViewAdListener() {
+            @Override
+            public void onAdExpanded(MaxAd ad) {
+
+            }
+
+            @Override
+            public void onAdCollapsed(MaxAd ad) {
+
+            }
+
+            @Override
+            public void onAdLoaded(MaxAd ad) {
+                Log.d("onAdLoaded", "onAdLoaded: ");
+            }
+
+            @Override
+            public void onAdDisplayed(MaxAd ad) {
+                Log.d("onAdLoaded", "onAdDisplayed: ");
+            }
+
+            @Override
+            public void onAdHidden(MaxAd ad) {
+                Log.d("onAdLoaded", "onAdHidden: ");
+            }
+
+            @Override
+            public void onAdClicked(MaxAd ad) {
+                Log.d("onAdLoaded", "onAdClicked: ");
+            }
+
+            @Override
+            public void onAdLoadFailed(String adUnitId, MaxError error) {
+                Log.d("onAdLoaded", "onAdLoadFailed: ");
+            }
+
+            @Override
+            public void onAdDisplayFailed(MaxAd ad, MaxError error) {
+
+                Log.d("onAdLoaded", "onAdDisplayFailed: ");
+            }
+        });
+
+        int width = AppLovinSdkUtils.dpToPx(this, 300);
+        int height = AppLovinSdkUtils.dpToPx(this, 250);
+        MRECAdview.setLayoutParams(new FrameLayout.LayoutParams(width, height, Gravity.CENTER));
+
+        MRECAdview.setBackgroundColor(Color.WHITE);
+
+        FrameLayout layout = findViewById(R.id.mrec);
+        layout.addView(MRECAdview);
+        MRECAdview.loadAd();
+        MRECAdview.startAutoRefresh();
+
+    }
+
 }
--- app/src/main/java/com/aniketjain/textscanner/SplashScreen.java
+++ app/src/main/java/com/aniketjain/textscanner/SplashScreen.java
@@ -1,14 +1,97 @@
 package com.aniketjain.textscanner;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.applovin.mediation.MaxAd;
+import com.applovin.mediation.MaxAdListener;
+import com.applovin.mediation.MaxError;
+import com.applovin.mediation.ads.MaxAppOpenAd;
+import com.applovin.sdk.AppLovinSdk;
+import com.github.ybq.android.spinkit.SpinKitView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private ExampleAppOpenManager appOpenManager;
+    private SpinKitView progressBar;
+    private TextView adLoadingTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+        progressBar = findViewById(R.id.spin_kit);
+        adLoadingTv = findViewById(R.id.ad_loading_tv);
+
+        AppLovinSdk.initializeSdk(this, configuration ->
+                appOpenManager = new ExampleAppOpenManager(SplashScreen.this));
+
     }
+
+    public class ExampleAppOpenManager implements MaxAdListener {
+        private final MaxAppOpenAd appOpenAd;
+        private final Context context;
+
+        public ExampleAppOpenManager(final Context context) {
+            this.context = context;
+            appOpenAd = new MaxAppOpenAd(Constant.APP_OPEN_KEY, context);
+            appOpenAd.setListener(this);
+            appOpenAd.loadAd();
+        }
+
+        private void showAdIfReady() {
+            if (appOpenAd == null || !AppLovinSdk.getInstance(context).isInitialized()) return;
+            if (appOpenAd.isReady()) {
+                progressBar.setVisibility(View.GONE);
+                adLoadingTv.setVisibility(View.GONE);
+                appOpenAd.showAd();
+            }
+        }
+
+
+        @Override
+        public void onAdLoaded(final MaxAd ad) {
+            Log.d("testAds", "ad loaded");
+            showAdIfReady();
+        }
+
+        @Override
+        public void onAdLoadFailed(final String adUnitId, final MaxError error) {
+            Log.d("testAds", "ad load fail");
+        }
+
+        @Override
+        public void onAdDisplayed(final MaxAd ad) {
+            Log.d("testAds", "ad display");
+        }
+
+        @Override
+        public void onAdClicked(final MaxAd ad) {
+            Log.d("testAds", "ad clicked");
+        }
+
+        @Override
+        public void onAdHidden(final MaxAd ad) {
+//            appOpenAd.loadAd();
+            Log.d("testAds", "ad hiddedn");
+            startActivity(new Intent(SplashScreen.this, OnboardingScreen.class));
+            finish();
+        }
+
+        @Override
+        public void onAdDisplayFailed(final MaxAd ad, final MaxError error) {
+            Toast.makeText(context, error.getMessage(), Toast.LENGTH_SHORT).show();
+            appOpenAd.loadAd();
+        }
+    }
+
 }
