d1bfd02725d58f84fddb58de4607127c95317677
umair4306 <mumair4306@gmail.com>
2022-11-03 17:45:38 +0500

    Image To Text

---
+++ app/src/main/java/com/aniketjain/textscanner/AppInfo.java
@@ -0,0 +1,15 @@
+package com.aniketjain.textscanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_info);
+
+    }
+}
---
+++ app/src/main/java/com/aniketjain/textscanner/Constant.java
@@ -0,0 +1,19 @@
+package com.aniketjain.textscanner;
+
+public class Constant {
+    public static String APP_OPEN_KEY="2a1eddedcf2c86e4";
+    public static String MREC_ADD_KEY="3b220f9810c33311";
+    public static String Package_Name = "com.humblyapps.compassdirection.qiblafinder.kaaba.direction";
+    public static String More_Apps_Link = "https://play.google.com/store/apps/dev?id=8637869990393747803";
+    public static String Rate_us_Link = "https://play.google.com/store/apps/details?id=com.humblyapps.compassdirection.qiblafinder.kaaba.direction";
+    public static String Policy_Link = "https://humblyapps.blogspot.com/p/humbly-apps-privacy-policy.html";
+    public static String License_Key = "LicenseKeyForIn-App";
+    public static String Admob_App_ID = "Admob(AppID)";
+    public static String Banner = "742cc5bbae7e0601";
+    public static String Interstitial = "Interstitial";
+    public static String Rewarded = "Rewarded";
+    public static String Rewarded_Interstitial = "Rewarded Interstitial";
+    public static String Native_Mrec = "d30e9332da4dbc47";
+    public static String App_Open = "d7704a0fd67679bd";
+    public static String Max_ID = "k6M5NTnJGpwkPtSKZe-eZAkAlOEfcw7nB1B-ayas5WK8Rl9Xprw89ufSwtGKOvMBVN-BGKx2za5dAW79ARe77H";
+}
